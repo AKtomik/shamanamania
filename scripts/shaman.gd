ca52f9b90029fe786c8c339402c0833b231b2a3a
aznoqmous <paul.landreau931v@gmail.com>
2026-01-31 12:35:50 +0100

    added correct mask feedback

--- scripts/shaman.gd
+++ scripts/shaman.gd
@@ -2,5 +2,12 @@ extends Node3D
 class_name Shaman
 
 @onready var mask_position: Node3D = $MaskPosition
+@onready var correct_mask_particles: GPUParticles3D = $CorrectMaskParticles
+
 var needed_mask : MaskResource
-var assigned_mask: Mask
+var assigned_mask: Mask :
+	set(value):
+		assigned_mask = value
+		if assigned_mask:
+			print(assigned_mask.mask_resource, " ", needed_mask, assigned_mask.mask_resource == needed_mask)
+		correct_mask_particles.emitting = assigned_mask.mask_resource == needed_mask
