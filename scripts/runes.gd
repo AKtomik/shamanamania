132b016c7b4d4fc18fb1b66df00caaf42cfab24d
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-02-01 13:52:35 +0100

    add runes to main script

--- scripts/runes.gd
+++ scripts/runes.gd
@@ -9,15 +9,18 @@ var historyDictionnary : Dictionary[String, levelMaskResourceHistory]
 func _ready() -> void:
 	historyDictionnary = {}
 
-func setHistory(levelId : String, history : Array[MaskResource]):
+func set_history(levelId : String, history : Array[MaskResource]) -> void:
 	var levelHistory = levelMaskResourceHistory.new()
 	levelHistory.array = history
 	historyDictionnary[levelId] = levelHistory
-	
-func addMaskToHistory(levelId : String, mask : MaskResource):
+
+func add_mask_to_history(levelId : String, mask : MaskResource) -> void:
 	var levelHistory = historyDictionnary[levelId]
 	levelHistory.array.append(mask)
 	historyDictionnary[levelId] = levelHistory
-	
-func getHistory(levelId : String):
+
+func get_history_object(levelId : String) -> levelMaskResourceHistory:
 	return historyDictionnary[levelId]
+	
+func get_history_parsed(levelId : String) -> String:
+	return "TODO"
